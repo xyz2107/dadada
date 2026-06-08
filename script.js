9ab50722fdba7fe1bc3d11a07438ffa152d61394
xyz2107 <74245190+xyz2107@users.noreply.github.com>
2026-06-08 22:19:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // --- Live Age Counter ---
-    const birthDate = new Date('2006-06-09T00:00:00');
+    const birthDate = new Date('2006-01-09T00:00:00');
     const countdownElement = document.getElementById('countdown');
 
     function updateAge() {
